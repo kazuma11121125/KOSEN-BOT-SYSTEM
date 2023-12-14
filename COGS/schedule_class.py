fa66c2df7b033430cfb8b5fd66693505a34f2c91
kazuma1112 <kazuma1112.mainmail@gmail.com>
2023-12-14 16:11:43 +0900

    定期送信の実装

--- COGS/schedule_class.py
+++ COGS/schedule_class.py
@@ -6,7 +6,6 @@ from discord import app_commands
 from student_schedule_manager import ClassScheduleManager,SubmissionManager,ClassuserSystem
 from discord import ui
 
-
 class Questionnaire(ui.Modal, title="教科入力"):
     def __init__(self, mode, class_name):
         super().__init__()
@@ -111,7 +110,14 @@ class schedule_class(commands.Cog):
         self.schedule_manager = ClassScheduleManager()
         self.submission_manager = SubmissionManager()
         self.user_info = ClassuserSystem()
-
+        self.send_dict = {
+            "1M":[1184742396492263454],
+            "1S":[1184742458781864007],
+            "1C":[1184753367289974794],
+            "1E":[1184753468620152922]
+        }
+        self.loop_start.start()
+        self.check = False
     @app_commands.command()
     async def add_basic(self,interaction:discord.Interaction):
         """
@@ -206,6 +212,48 @@ class schedule_class(commands.Cog):
         embed.add_field(name="`/homework_view`",value="```宿題確認```",inline=False)
         embed.add_field(name="このBotについて",value="このBotはkazuma1112 M2303によって無償開発されています。\n開発支援金はこちらまで\nPayPay ID:kazuma11112\nKyash:ID kazuma1112\n開発費として使用します。\n開発支援-サーバー提供 [S-Server Developers](https://sdev.aknet.tech/)",inline=False)
         await interaction.response.send_message(embed=embed)
+
+    @tasks.loop(seconds=15)
+    async def loop_start(self):
+        try:
+            now = datetime.now()
+            if now.strftime("%H:%M") == "16:30" and self.check == False:
+                lists = ["1M","1S","1C","1E"]
+                for name in lists:
+                    today = datetime.now().date()
+                    tomorrow = today + timedelta(days=1)
+                    tomorrow = str(tomorrow)
+                    result = await self.schedule_manager.view_class_schedule(name, tomorrow)
+                    upcoming_submissions = await self.submission_manager.get_upcoming_submissions(name)
+                    cont = 1
+                    embed = discord.Embed(color=0x2ecc71,title=f"{name}スケジュール")
+                    if result:
+                        embed.add_field(name=f"-----明日({tomorrow}) 講義日程-----",value="",inline=False)
+                        for i in result:
+                            embed.add_field(name=f"{cont}限目",value=i,inline=False)
+                            cont = cont+1
+                    else:
+                        embed = discord.Embed(color=0x2ecc71,title=f"{name} 講義日程未登録")
+                    embed.add_field(name="-----課題状況-----",value="",inline=False)
+                    if upcoming_submissions:
+                        for deadline, assignments in upcoming_submissions.items():
+                            remaining_days = await self.submission_manager.count_remaining_days(deadline)
+                            ass = ""
+                            for txt  in assignments:
+                                ass += txt
+                                ass += "\n" 
+                                
+                            embed.add_field(name=f"{deadline} 残り:{remaining_days}日",value=f"```{ass}```\n\n",inline=False)
+                    else:
+                        embed.add_field(name="課題情報未登録",value="",inline=False)
+                    for id in self.send_dict[name]:
+                        if id:   
+                            await self.bot.get_channel(id).send(embed=embed)
+                self.check = True
+            if now.strftime("%H:%M") == "16:31":
+                self.check = False
+        except Exception as e:
+            await self.bot.get_channel(id).send(f"An error occurred: {type(e).__name__} - {e}")        
 
 async def setup(bot):
     await bot.add_cog(schedule_class(bot))
