a071a3c0d124c5a92e6d0b48e57adb9e2be0fcc6
kazuma1112 <kazuma1112.bakabaa.ahoaho@gmail.com>
2024-01-28 17:42:31 +0900

    変更

--- COGS/youtube.py
+++ COGS/youtube.py
@@ -3,9 +3,12 @@ from discord.ext import commands
 import yt_dlp as youtube_dl
 import asyncio
 import os
+import random
+
 class MusicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.played_history = set()  # 再生した曲の履歴を管理するセット
 
     async def download_audio(self, url):
         ydl_opts = {
@@ -23,33 +26,55 @@ class MusicCog(commands.Cog):
 
         return info["title"]
 
-    @commands.command()
-    async def download(self, ctx, url):
-        title = await self.download_audio(url)
-        await ctx.send(f'Download complete: {title}.mp3')
-
-    @commands.command()
-    async def play(self, ctx):
-        channel = ctx.message.author.voice.channel
-        voice_channel = await channel.connect()
-
-        # ダウンロード先のディレクトリから、最新のMP3ファイルを取得
-        mp3_files = [f for f in os.listdir('downloads') if f.endswith('.mp3')]
+    async def play_random_mp3(self, voice_channel):
+        # ダウンロード先のディレクトリから、MP3ファイルを取得
+        mp3_files = [f for f in os.listdir('downloads') if f.endswith('.mp3') and f not in self.played_history]
         if mp3_files:
-            mp3_file = max(mp3_files, key=lambda x: os.path.getctime(os.path.join('downloads', x)))
+            mp3_file = random.choice(mp3_files)
 
             # MP3 ファイルを再生
             voice_channel.play(discord.FFmpegPCMAudio(f'downloads/{mp3_file}'))
+
+            # 再生履歴に追加
+            self.played_history.add(mp3_file)
 
             # 再生完了まで待機
             while voice_channel.is_playing():
                 await asyncio.sleep(1)
 
-            # 接続を切断
-            await asyncio.sleep(1)  # 1秒待機
-            await voice_channel.disconnect()
+            return True
         else:
-            await ctx.send('No MP3 files found to play.')
+            return False
+
+    @commands.command()
+    async def download(self, ctx, url):
+        await ctx.send("Start")
+        title = await self.download_audio(url)
+        await ctx.send(f'Download complete: {title}.mp3')
+
+    @commands.command()
+    async def play_all(self, ctx):
+        channel = ctx.message.author.voice.channel
+        voice_channel = await channel.connect()
+
+        while await self.play_random_mp3(voice_channel):
+            pass  # ファイルがある限り再生を続ける
+
+        # 全ての再生が終わったらボイスチャンネルから切断
+        await asyncio.sleep(1)  # 必要に応じて調整
+        await voice_channel.disconnect()
+        await ctx.send('All MP3 files played. Bot disconnected.')
+
+    @commands.command()
+    async def skip(self, ctx):
+        # スキップ時に再生履歴をクリア
+        self.played_history.clear()
+
+        # ボイスチャンネルから切断
+        voice_channel = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        if voice_channel:
+            await voice_channel.disconnect()
+            await ctx.send('Skipped. Bot disconnected.')
 
 
 async def setup(bot):
